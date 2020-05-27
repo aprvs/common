3f4b4ce79a6d5b29473af0913c00567e51ba0d90
aprvs <apoorvs92@gmail.com>
2020-05-26 21:31:03 -0700

    Create a representative enum trait test
    Some things to consider when improving this feature
    -defining an enum and immediately using its traits is not possible
    -macros arent really a feasible solution for this code gen problem

--- enum_traits_test.cc
+++ enum_traits_test.cc
@@ -3,7 +3,7 @@
 #include <utility>
 #include "gtest/gtest.h"
 
-namespace common {
+namespace test_namespace {
 
 namespace {
 
@@ -11,57 +11,68 @@ enum class MyEnum1 { kA, kB, kC, kD, kE };
 
 enum MyEnum2 { kMyEnum2A, kMyEnum2B, kMyEnum2C };
 
-class MyClass {};
-
 }  // namespace
+}  // test_namespace
+
+namespace common {
 
 template <>
-MyEnum1 EnumTrait<MyEnum1>::default_value() {
-  return MyEnum1::kD;
-}
-
-template <>
-constexpr size_t EnumTrait<MyEnum1>::num_values() {
-  return 5;
-}
-
-template <>
-const char* EnumTrait<MyEnum1>::to_string(MyEnum1 e) {
-  switch (e) {
-    case MyEnum1::kA:
-      return "A";
-    case MyEnum1::kB:
-      return "B";
-    case MyEnum1::kC:
-      return "C";
-    case MyEnum1::kD:
-      return "D";
-    case MyEnum1::kE:
-      return "E";
+struct EnumTrait<test_namespace::MyEnum1> {
+  constexpr static test_namespace::MyEnum1 default_value() {
+    return test_namespace::MyEnum1::kD;
   }
-  return "Unknown";
+  constexpr static size_t num_values() { return 5; }
+  constexpr static const char* to_string(test_namespace::MyEnum1 e) {
+    switch (e) {
+      case test_namespace::MyEnum1::kA:
+        return "A";
+      case test_namespace::MyEnum1::kB:
+        return "B";
+      case test_namespace::MyEnum1::kC:
+        return "C";
+      case test_namespace::MyEnum1::kD:
+        return "D";
+      case test_namespace::MyEnum1::kE:
+        return "E";
+    }
+    return "Unknown";
+  }
+};
+
+template <>
+std::array<test_namespace::MyEnum1,
+           EnumTrait<test_namespace::MyEnum1>::num_values()>
+EnumListTrait<test_namespace::MyEnum1>::values() {
+  return {{test_namespace::MyEnum1::kA, test_namespace::MyEnum1::kB,
+           test_namespace::MyEnum1::kC, test_namespace::MyEnum1::kD,
+           test_namespace::MyEnum1::kE}};
 }
 
 template <>
-std::array<MyEnum1, EnumTrait<MyEnum1>::num_values()>
-EnumListTrait<MyEnum1>::values() {
-  return {{MyEnum1::kA, MyEnum1::kB, MyEnum1::kC, MyEnum1::kD, MyEnum1::kE}};
+test_namespace::MyEnum2 EnumTrait<test_namespace::MyEnum2>::default_value() {
+  return test_namespace::kMyEnum2B;
 }
 
-template <>
-MyEnum2 EnumTrait<MyEnum2>::default_value() {
-  return kMyEnum2B;
-}
+}  // namespace  common
+namespace test_namespace {
 
 TEST(EnumTraitsTest, DefaultValueTest) {
-  EXPECT_EQ(EnumTrait<MyEnum1>::default_value(), MyEnum1::kD);
-  EXPECT_EQ(EnumTrait<MyEnum2>::default_value(), kMyEnum2B);
+  EXPECT_EQ(common::EnumTrait<MyEnum1>::default_value(), MyEnum1::kD);
+  EXPECT_EQ(common::EnumTrait<MyEnum2>::default_value(), kMyEnum2B);
 }
 
 TEST(EnumTraitsTest, ArrayTest) {
-  for (MyEnum1 val : EnumListTrait<MyEnum1>::values()) {
-    std::cout << EnumTrait<MyEnum1>::to_string(val) << std::endl;
-  }
+  const auto& values = common::EnumListTrait<MyEnum1>::values();
+  EXPECT_EQ(values[0], MyEnum1::kA);
+  EXPECT_EQ(common::EnumTrait<MyEnum1>::to_string(values[0]), "A");
+  EXPECT_EQ(values[1], MyEnum1::kB);
+  EXPECT_EQ(common::EnumTrait<MyEnum1>::to_string(values[1]), "B");
+  EXPECT_EQ(values[2], MyEnum1::kC);
+  EXPECT_EQ(common::EnumTrait<MyEnum1>::to_string(values[2]), "C");
+  EXPECT_EQ(values[3], MyEnum1::kD);
+  EXPECT_EQ(common::EnumTrait<MyEnum1>::to_string(values[3]), "D");
+  EXPECT_EQ(values[4], MyEnum1::kE);
+  EXPECT_EQ(common::EnumTrait<MyEnum1>::to_string(values[4]), "E");
 }
 
-}  // namespace common
+}  // namespace test_namespace
